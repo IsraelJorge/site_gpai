8da28c65bbc232e827fbe808fb2381d46d8e9d35
IsraelJorge <israeljorge2000@gmail.com>
2023-05-15 19:32:10 -0300

    feat: add component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from './components/Button';
 import { Header } from './components/Header';
 
 export function App() {
   return (
     <>
       <Header />
+      <Button.Root as="Link">
+        <Button.Label>fsdffsdfs</Button.Label>
+      </Button.Root>
     </>
   );
 }
