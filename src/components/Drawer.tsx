f3b94a8ae58660b54b77c51d3ed670955af0541f
IsraelJorge <israeljorge2000@gmail.com>
2023-05-23 18:33:33 -0300

    feat: add component drawer

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,14 @@
+export function DrawerRoot() {
+  return (
+    <div className="drawer-side">
+      <ul className="menu p-4 w-80  h-screen bg-secondary  text-base-content">
+        <li>
+          <a>Sidebar Item 1</a>
+        </li>
+        <li>
+          <a>Sidebar Item 2</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,22 +1,11 @@
 import { Button } from '../components/Button';
+import { DrawerRoot } from '../components/Drawer';
 import { Input } from '../components/Input';
 
 export function About() {
   return (
     <>
-      <h1>Sobre</h1>
-
-      <div className="p-10">
-        <Input label="Nome" name="name" />
-
-        <Input label="Telefone" name="cell" />
-
-        <Input label="asda" name="qweqwe" />
-
-        <Button variant="outline" className="w-44">
-          <Button.Label>Entrar</Button.Label>
-        </Button>
-      </div>
+      <DrawerRoot />
     </>
   );
 }
